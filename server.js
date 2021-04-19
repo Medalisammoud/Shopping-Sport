270d90bd42096acb02edc1592c186a7a12c047ac
Mohamed Ali Sammoud <Mohamed.Ali.Sammoud@gmail.com>
2021-04-19 15:09:09 +0100

    Modif

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ app.use("/api/product", require("./routes/product"));
 app.use("/api/order", require("./routes/order"));
 app.use("/api/favorite", require("./routes/favorite"));
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+    app.get("*",(req , res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 //Connect Server
 app.listen(PORT,(err) =>
 err ? console.error(err) 
